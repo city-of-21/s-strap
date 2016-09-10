945fd74f0a8dacf994d91f2d2587bd13f68bae86
city-of-21 <ken21city@hotmail.com>
2016-09-10 13:38:59 +0900

    1.2.0

--- js/ng-items.js
+++ js/ng-items.js
@@ -11,8 +11,7 @@ var navLoopCtrl = function($scope) {
         {"class":"active","href":baseUrl,"text":"Home","_blank":"false","dropdown":"false"},
         {"class":"","href":baseUrl+"about.html","text":"会社概要","_blank":"false","dropdown":"false"},
         {"class":"","href":baseUrl+"askme.html","text":"お問い合わせ","_blank":"false","dropdown":"false"},
-        {"class":"","href":"#","text":"リリースアプリ","_blank":"true","dropdown":"true","navchildren":[{"href":"https://itunes.apple.com/jp/app/practice-camera-xie-zhen-gou/id954811892?mt=8&uo=4","text":"practice-camera"},{"href":"https://itunes.apple.com/jp/app/id979555997?mt=8&ign-mpt=uo%3D4","text":"リッスン to the 外国語"}]},
         {"class":"","href":"#","text":"サンプルサイト","_blank":"false","dropdown":"true","navchildren":[{"href":baseUrl+"yamazon/","text":"yamazon"},{"href":baseUrl+"parapallax/","text":"parapallax"},{"href":baseUrl+"canvas/","text":"canvaslife"},{"href":baseUrl+"worldclock/","text":"worldclock"}]},
-		{"class":"","href":"#","text":"ソーシャルメディア","_blank":"true","dropdown":"true","navchildren":[{"href":"https://twitter.com/OEDORoulette","text":"開発者Twitter"},{"href":"http://shiftswift.blogspot.jp/","text":"Swift体験記"},{"href":"http://fc-salware.blogspot.jp/","text":"FC salware"}]},
+		{"class":"","href":"#","text":"ソーシャルメディア","_blank":"true","dropdown":"true","navchildren":[{"href":"https://twitter.com/OEDORoulette","text":"開発者Twitter"}]},
     ];
 }
